59433161ab8b33370ba8782dd8e644c65aaec3cd
Ashis_Dubug <tripathychintu000@gmail.com>
2024-04-11 21:59:33 +0530

    Added a Unique key based on index

--- web_app.py
+++ web_app.py
@@ -14,13 +14,12 @@ st.title("My Todo App")
 st.subheader("This is a minimal todo application")
 st.write("This app will increase your productivity.")
 
-for index,todo in enumerate(todos):
-    check = st.checkbox(todo, key=todo)
+
+for index, todo in enumerate(todos):
+    check = st.checkbox(todo, key=f"checkbox_{index}")  # Unique key based on index
     if check:
         todos.pop(index)
         fun.update_todos(todos)
-        del st.session_state[todo]
         st.rerun()
 
 st.text_input(label='', placeholder='Write new TODO to add...', on_change=add_todo, key='new_todo')
-
